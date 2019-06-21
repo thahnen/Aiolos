14700895f21c3117b0faabf833ddbb7b00683c4b
Tobias Hahnen <hahnen.tobi@gmail.com>
2019-06-21 13:30:09 +0200

    Added implementations
    Added the (neccessary assertions), added a test, which does not work yet sadly.
    TODO: debugging and looking for mathematical mistakes! making a somewhat nice readme :o

--- Aiolos/main.cpp
+++ Aiolos/main.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 
 /**************************************************************************************************
  *
- *      HELPING FUNCTIONS
+ *      HELPER FUNCTIONS
  *
  **************************************************************************************************/
 unsigned int max_gray_value(Mat image) {
@@ -40,19 +40,64 @@ class Scheme {
 public:
     // Function to calculate the dominant texture orientation
     unsigned int min_theta(Mat image) {
+        // Assertion: only one channel!
+        assert(image.channels() == 1);
+
         vector<unsigned int> values;
 
+        // Assertion: only values 0 <= x < 360
         for (int theta = 0; theta < 360; ++theta) {
             values.push_back(Z_(image, theta));
         }
 
         unsigned int min_value = *min_element(values.begin(), values.end());
 
-        // Then use arg(min_value) but arg is for complex numbers?
+        return arg(min_value);
     }
 
     virtual ~Scheme() = default;
-private:
+
+protected:
+    // GLCM matrix element (m,n) using relative position (r,theta)
+    // => extended by Scheme 1+2!
+    // => changed only by Scheme 3!
+    unsigned int G_(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta) {
+        unsigned int q = Q_(image, r, theta);
+        unsigned int value = 0;
+
+        for (int i = 0; i < image.cols; ++i) {
+            for (int j = 0; j < image.rows; ++j) {
+                unsigned int x = i + r*cos(theta);
+                unsigned int y = j + r*sin(theta);
+
+                // Possible cast from smaller to higher value at "image.at<unsigned int>(...)" maybe not 100% efficient
+                if (x >= 0 && x < image.cols && y >= 0 && y < image.cols
+                    && image.at<unsigned int>(i, j) == m-1 && image.at<unsigned int>(x, y) == n-1) {
+                    value++;
+                }
+            }
+        }
+
+        // return GLCM-Matrix element
+        return value;
+    }
+
+    // Normalization factor
+    // => changed only for Scheme 3!
+    unsigned int Q_(Mat image, double r, unsigned int theta) {
+        int value = 0;
+        for (int i = 0; i < image.cols; ++i) {
+            for (int j = 0; j < image.rows; ++j) {
+                unsigned x = i + r*cos(theta);
+                unsigned y = j + r*sin(theta);
+
+                if (x >= 0 && x < image.cols && y >= 0 && y < image.rows) {
+                    value++;
+                }
+            }
+        }
+    }
+
     // increasing function of the spacing between matrix element (m,n) and the diagonal line
     unsigned int w(unsigned int m, unsigned int n) {
         return pow(m-n, 2);
@@ -63,6 +108,7 @@ private:
         unsigned int value = 0;
         unsigned int depth = max_gray_value(image);
 
+        // Assertion: Matrix-Element (n,m) in Bounds!
         for (int i = 1; i < depth; ++i) {
             for (int j = 1; j < depth; ++j) {
                 value += w(i, j) * G(image, i, j, r, theta);
@@ -77,6 +123,7 @@ private:
         unsigned int value = 0;
         unsigned int max_r = ceil(max(image.rows, image.cols) * sqrt(2)); // max radius to get the corners
 
+        // Assertion: 1 <= r < max_r
         for (int r = 1; r < max_r; ++r) {
             value += Z(image, r, theta);
         }
@@ -84,9 +131,8 @@ private:
         return value;
     }
 
-    // Abstract methods to be implemented by the different schemes
+    // Abstract method to be implemented by the different schemes
     virtual unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta) = 0;
-    virtual unsigned int Q(Mat image, double r, unsigned int theta) = 0;
 };
 
 
@@ -95,28 +141,37 @@ private:
  *      SCHEME 1 IMPLEMENTATION
  *
  **************************************************************************************************/
-class Scheme1 : Scheme {
+class Scheme1 : public Scheme {
 private:
     // GLCM matrix element (m,n) using relative position (r,theta)
-    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta);
-
-    // Normalization factor
-    unsigned int Q(Mat image, double r, unsigned int theta);
+    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta) {
+        return (floor(r) + 1 - r) * G_(image, m, n, r, 0) + (r - floor(r)) * G_(image, m, n, r, 0);
+    };
 };
 
 
 /**************************************************************************************************
  *
  *      SCHEME 2 IMPLEMENTATION
+ *      => should be done!
  *
  **************************************************************************************************/
-class Scheme2 : Scheme {
+class Scheme2 : public Scheme {
 private:
     // GLCM matrix element (m,n) using relative position (r,theta)
-    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta);
+    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta) {
+        double c_1 = (floor(r*cos(theta)) + 1 - r*cos(theta)) * (floor(r*sin(theta)) + 1 - r*sin(theta));
+        double c_2 = (r*cos(theta) - floor(r*cos(theta))) * (floor(r*sin(theta)) + 1 - r*sin(theta));
+        double c_3 = (floor(r*cos(theta)) + 1 - r*cos(theta)) * (r*sin(theta) - floor(r*sin(theta)));
+        double c_4 = (r*cos(theta) - floor(r*cos(theta))) * (r*sin(theta) - floor(r*sin(theta)));
 
-    // Normalization factor
-    unsigned int Q(Mat image, double r, unsigned int theta);
+        unsigned int g_1 = G_(image, m, n, floor(r*cos(theta)), floor(r*sin(theta)));
+        unsigned int g_2 = G_(image, m, n, floor(r*cos(theta)) + 1, floor(r*sin(theta)));
+        unsigned int g_3 = G_(image, m, n, floor(r*cos(theta)), floor(r*sin(theta)) + 1);
+        unsigned int g_4 = G_(image, m, n, floor(r*cos(theta)) + 1, floor(r*sin(theta)) + 1);
+
+        return c_1*g_1 + c_2*g_2 + c_3*g_3 + c_4*g_4;
+    }
 };
 
 
@@ -125,13 +180,19 @@ private:
  *      SCHEME 3 IMPLEMENTATION
  *
  **************************************************************************************************/
-class Scheme3 : Scheme {
+class Scheme3 : public Scheme {
 private:
     // GLCM matrix element (m,n) using relative position (r,theta)
-    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta);
+    [[deprecated("Not implemented yet!")]]
+    unsigned int G(Mat image, unsigned int m, unsigned int n, double r, unsigned int theta) {
+        return -1;
+    }
 
     // Normalization factor
-    unsigned int Q(Mat image, double r, unsigned int theta);
+    [[deprecated("Not implemented yet!")]]
+    unsigned int Q(Mat image, double r, unsigned int theta) {
+        return -1;
+    }
 };
 
 
@@ -139,9 +200,22 @@ private:
 /**************************************************************************************************
  *
  *      MAIN FUNCTION FOR TESTING
+ *      TODO: does run infinitly?
  *
  **************************************************************************************************/
 int main() {
-    cout << "Hello, World!" << endl;
+    Mat image = imread("/home/thahnen/Downloads/zebrastreifen.jpg");
+    imshow("Zebrastrifen:", image);
+
+    Mat gray_image = image.clone();
+    cvtColor(gray_image, gray_image, COLOR_BGR2GRAY);
+    imshow("Zebrastreifen (Grayscale):", gray_image);
+    waitKey(0);
+
+    Scheme2 scheme;
+    cout << "Winkel suchen ..." << endl;
+    cout << "Winkel gefunden: " << scheme.min_theta(gray_image) << endl;
+
+    waitKey(0);
     return 0;
 }
